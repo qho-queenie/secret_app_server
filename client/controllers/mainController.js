9917e05bc130ff3999e5a6c2cd9ff5206b644647
Harmann Bajwa <harmannbajwa@Harmanns-MacBook-Pro.local>
2017-02-11 19:51:27 -0800

    modified partials homepage and account

--- client/controllers/mainController.js
+++ client/controllers/mainController.js
@@ -1,12 +1,20 @@
-app.controller("panelController", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http){
+app.controller("mainController", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http){
   $scope.users = [];
   $scope.contacts = [];
   $scope.addUser = function() {
     $scope.users.push($scope.newUser);
-    $scope.newUser = {};
+    $http.post('/registration', $scope.newUser).then(function(res){
+      console.log(res);
+    })
+    window.location.href = "#/account";
   }
   $scope.addContact = function(){
     $scope.contacts.push($scope.newContact);
-    $scope.newContact = {};
+    $http.post('/add_new_contact', $scope.newContact).then(function(res){
+      console.log(res);
+    })
+    window.location.href = "#/account";
   }
 }])
+
+
