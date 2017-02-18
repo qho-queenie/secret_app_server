645482cd8ce665e4d9ebedbb9722c1eeb87de9ba
Chris Rollins <chris.rollins@leapfire.com>
2017-02-17 17:52:33 -0800

    edit profile

--- server/models/model_template.js
+++ server/models/model_template.js
@@ -87,10 +87,10 @@ module.exports = {
 	edit_profile: function(req, res, callback){
 		var first_name = (req.body.first_name)?`first_name = ${req.body.first_name}, `:"";
 		var last_name = (req.body.last_name)?`last_name = ${req.body.last_name}, `:"";
-		var email = (req.body.email)?`email = {req.body.email}, `:"";
-		var phone = (req.body.phone)?`phone = {req.body.phone}, `:"";
+		var email = (req.body.email)?`email = ${req.body.email}, `:"";
+		var phone = (req.body.phone)?`phone = ${req.body.phone}, `:"";
 		var password = (req.body.password)?`password = {req.body.password}, `:"";
-		var doq = `UPDATE users SET ${first_name}${last_name}${email}{$phone}updated_at=NOW() WHERE id=${req.session.data.id}`;
+		var doq = `UPDATE users SET ${first_name}${last_name}${email}${phone}updated_at=NOW() WHERE id=${req.session.data.id}`;
 		console.log(doq);
 		doQuery(doq, callback);
 	}
