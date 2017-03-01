e188c944f9bbe0a4d36776a7610d026af50014b9
Chris Rollins <chris.rollins@leapfire.com>
2017-02-28 16:29:46 -0800

    cleanup contacts

--- server/config/hourlyLoop.js
+++ server/config/hourlyLoop.js
@@ -1,3 +1,5 @@
+var models = require("./../models/model_template.js");
+
 module.exports = function(){
 	var day = new Date().getDay();
 	var hours = new Date().getUTCHours();
@@ -6,5 +8,8 @@ module.exports = function(){
 
 	if(day == 0 && hour == 0){
 		console.log("time to cleanup declined contacts");
+		models.model_template.remove_declined_contacts(undefined, undefined, function(err, rows){
+			console.log(`result of cleanup: ${rows}`);
+		})
 	}
 }
