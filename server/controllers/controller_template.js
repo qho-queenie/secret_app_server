e94aca2603e2716009a22c5c22a4c83e4c5c371d
Queenie Ho <qho.queenieho@gmail.com>
2017-03-04 23:09:06 -0800

    start task sms ternary 1

--- server/controllers/controller_template.js
+++ server/controllers/controller_template.js
@@ -98,7 +98,8 @@ exps = {
     	if(!phone)
     		phone = hardcodedPhoneNumber;
 			console.log(data, "is the data passed correctly?");
-		flowroute.MessagesController.createMessage({"to": phone, "from": "14089122921", "content": `${data.user_first_name} is starting task ${data.event_name}. You will be alerted again if they don't check in after ${data.minutes} minutes. ${data.user_first_name} also said that ${data.additional_message}`}, function(err, response){
+
+		flowroute.MessagesController.createMessage({"to": phone, "from": "14089122921", "content": `${data.user_first_name} is starting task ${data.event_name}. You will be alerted again if they don't check in after ${data.minutes} minutes. ${(data.additional_message)? `${data.user_first_name} also said that ${data.additional_message}` : ""}`}, function(err, response){
 		      if(err){
 		        console.log(err);
 						// res.json({success: false, validation_errors: ["Something went wrong. Please try again."]});
