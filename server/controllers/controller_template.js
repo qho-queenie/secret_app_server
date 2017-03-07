16c59a56039eed856aef887f25a4acf29f44d717
Chris Rollins <chris.rollins@leapfire.com>
2017-03-07 00:26:13 -0800

    fixing availability still

--- server/controllers/controller_template.js
+++ server/controllers/controller_template.js
@@ -264,6 +264,7 @@ exps = {
 			{	
 				models.model_template.get_contact_avail_info_from_crypto(crypto_code, function(err, rows, fields){
 					var id = rows[0].contact_id;
+					console.log(rows);
 					contact_availability[id] = true;
 					var duration = 3600000;
 					setTimeout(function(){contact_availability[id] = undefined;}, duration);
