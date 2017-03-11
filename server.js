7c2eb351b4bc21f20c152f32f047a07394369523
Chris Rollins <chris.rollins@leapfire.com>
2017-03-11 15:32:29 -0800

    allow any origin for now

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.use(session({
 }));
 
 app.use(function(req, res, next) {
-	console.log(req.get("origin"));
-	res.header("Access-Control-Allow-Origin", "*");
+	const origin = req.get("origin");
+	res.header("Access-Control-Allow-Origin", origin);
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
